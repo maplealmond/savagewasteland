e30ce4d9951b6143b3f8e2f7a50405ff11f8d774
Michael Amygdalidis <malmond@cmgecm.com>
2020-02-13 13:48:49 -0600

    Item updates

--- decks/items/misc_deck.rb
+++ decks/items/misc_deck.rb
@@ -1,7 +1,7 @@
 require_relative 'item.rb'
 require_relative '../../models/misc'
 
-data = Misc.new(Squib.xlsx file: 'data/misc.xlsx')
+data = Misc.new(Squib.xlsx file: 'data/misc.xlsx', explode: "Qty")
 Squib::Deck.new(cards: data.name.size, layout: LAYOUTS) do
   common(self, data)
 
